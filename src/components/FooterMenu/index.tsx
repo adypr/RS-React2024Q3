c84701c09553435162d47aa75efe2eecd19751f5
adypr <workady2015@gmail.com>
2024-07-18 16:59:06 +0300

    feat: add progressbar into footer-menu

--- src/components/FooterMenu/index.tsx
+++ src/components/FooterMenu/index.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 import { clearSelection } from '../../store/slices/astronomicalObjectsSlice';
+import './FooterMenu.scss';
 
 const FooterMenu: React.FC = () => {
   const dispatch = useDispatch();
@@ -9,6 +10,8 @@ const FooterMenu: React.FC = () => {
     (state: RootState) => state.astronomicalObjects.selectedItems
   );
   const [csvContent, setCsvContent] = useState('');
+  const [downloadProgress, setDownloadProgress] = useState(0);
+  const [isDownloading, setIsDownloading] = useState(false);
   const linkRef = useRef<HTMLAnchorElement | null>(null);
 
   const handleClearSelection = () => {
@@ -31,18 +34,40 @@ const FooterMenu: React.FC = () => {
       .join('\n');
 
     setCsvContent(`data:text/csv;charset=utf-8,${csv}`);
+    setIsDownloading(true);
   };
 
   useEffect(() => {
     if (csvContent && linkRef.current) {
       linkRef.current.click();
       setCsvContent('');
+      setTimeout(() => {
+        let progress = 0;
+        const interval = setInterval(() => {
+          progress += 10;
+          setDownloadProgress(progress);
+          if (progress >= 100) {
+            clearInterval(interval);
+            setTimeout(() => {
+              setIsDownloading(false);
+              setDownloadProgress(0);
+            }, 10000);
+          }
+        }, 100);
+      }, 1000);
     }
   }, [csvContent]);
 
   return (
     <div className="footer-menu">
       <button onClick={handleClearSelection}>Отменить выбор всех</button>
+      {isDownloading && (
+        <div className="progress-bar">
+          <div className="progress" style={{ width: `${downloadProgress}%` }}>
+            {downloadProgress}%
+          </div>
+        </div>
+      )}
       <button onClick={handleDownload}>Загрузить</button>
       <a
         ref={linkRef}
