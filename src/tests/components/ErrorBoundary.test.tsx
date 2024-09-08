7fbd98b2ccb9c69bf1214d9eb34029e0a5c98bd3
adypr <workady2015@gmail.com>
2024-09-08 16:54:52 +0300

    refactor: mock console.error for ErrorBoundary test

--- src/tests/components/ErrorBoundary.test.tsx
+++ src/tests/components/ErrorBoundary.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { vi } from 'vitest';
 import ErrorBoundary from '../../components/ErrorBoundary';
 import EmulateErrorComponent from '../../components/EmulateErrorComponent';
 
@@ -16,6 +17,9 @@ describe('ErrorBoundary Component', () => {
   });
 
   it('should display error message when an error is thrown', () => {
+    const originalError = console.error;
+    console.error = vi.fn();
+
     render(
       <ErrorBoundary>
         <EmulateErrorComponent />
@@ -24,5 +28,7 @@ describe('ErrorBoundary Component', () => {
 
     expect(screen.getByText('Something went wrong.')).toBeInTheDocument();
     expect(screen.getByText('An unexpected error occurred. Please try reloading this component.')).toBeInTheDocument();
+
+    console.error = originalError;
   });
 });
